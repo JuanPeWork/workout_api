59daeeeac5cc22bb1af7a7eaab47e6c586fb014e
jpadamuz <juanpework@gmail.com>
2020-02-07 21:37:07 +0100

    Delete old cors middleware

--- routes/api.php
+++ routes/api.php
@@ -17,48 +17,45 @@ use App\Http\Middleware\ApiAuthMiddleware;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::get('', ['middleware' => 'cors', function() {
 
-    //USER
-    Route::post('user/create', 'UserController@create');
-    Route::post('user/login', 'UserController@login');
-    //?  function Editar
-    //? function Leer
+//USER
+Route::post('user/create', 'UserController@create');
+Route::post('user/login', 'UserController@login');
+//?  function Editar
+//? function Leer
 
 
-    //WORKOUT
-    Route::post('user/{user_id}/workout/create', 'WorkoutController@create')->middleware(ApiAuthMiddleware::class);
-    Route::get('user/{user_id}/workouts', 'WorkoutController@getUserWorkouts')->middleware(ApiAuthMiddleware::class);
-    Route::get('user/{user_id}/workouts/{id}', 'WorkoutController@getWorkout')->middleware(ApiAuthMiddleware::class);
-    //? function Editar
-    //? function Eliminar
+//WORKOUT
+Route::post('user/{user_id}/workout/create', 'WorkoutController@create')->middleware(ApiAuthMiddleware::class);
+Route::get('user/{user_id}/workouts', 'WorkoutController@getUserWorkouts')->middleware(ApiAuthMiddleware::class);
+Route::get('user/{user_id}/workouts/{id}', 'WorkoutController@getWorkout')->middleware(ApiAuthMiddleware::class);
+//? function Editar
+//? function Eliminar
 
-    //WORKOUT_TYPE
-    Route::get('workout-type', 'WorkoutTypeController@listWorkoutType')->middleware(ApiAuthMiddleware::class);
+//WORKOUT_TYPE
+Route::get('workout-type', 'WorkoutTypeController@listWorkoutType')->middleware(ApiAuthMiddleware::class);
 
-    //TRAINING_SESSION
-    //! Crear
-    Route::post('/training-session', 'TrainingSessionController@create')->middleware(ApiAuthMiddleware::class);
+//TRAINING_SESSION
+//! Crear
+Route::post('/training-session', 'TrainingSessionController@create')->middleware(ApiAuthMiddleware::class);
 
-    //! Leer
-    Route::get('workouts/{workout_id}/training-session/', 'TrainingSessionController@getTrainingSessionWorkout')->middleware(ApiAuthMiddleware::class);
+//! Leer
+Route::get('workouts/{workout_id}/training-session/', 'TrainingSessionController@getTrainingSessionWorkout')->middleware(ApiAuthMiddleware::class);
 
-    //? Eliminar
-    //? Editar
+//? Eliminar
+//? Editar
 
-    //TRAINING_SESSION_TYPE
-    Route::get('training-session-type', 'TrainingSessionTypeController@listTrainingSessionType')->middleware(ApiAuthMiddleware::class);
+//TRAINING_SESSION_TYPE
+Route::get('training-session-type', 'TrainingSessionTypeController@listTrainingSessionType')->middleware(ApiAuthMiddleware::class);
 
-    
-    //Exercise
-    //! Crear 
-    //! Leer 
-    //? Editar
-    //? Eliminar
+//Exercise
+//! Crear 
+//! Leer 
+//? Editar
+//? Eliminar
 
-    //Exercise_type
-    //* Leer
+//Exercise_type
+//* Leer
 
-    // Muscle Group
-    //? Leer
-}]);
+// Muscle Group
+//? Leer
