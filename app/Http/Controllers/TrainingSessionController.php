654aa354bbe756b28fc4d12b0732a5d93f07ca03
JuanPeWork <juanpework@gmail.com>
2022-02-20 18:59:52 +0100

    Update SQL getTrainingSession

--- app/Http/Controllers/TrainingSessionController.php
+++ app/Http/Controllers/TrainingSessionController.php
@@ -9,8 +9,8 @@ class TrainingSessionController extends Controller
 {
     //Obtener las sesiones de entrenamiento de la rutina
     public function getTrainingSessionWorkout($workout_id){
-        $trainingSession = TrainingSession::join('session_type', 'session_type.id', '=', 'session.session_type_id')
-                            ->select('session.*', 'session_type.name as type_name')
+        $trainingSession = TrainingSession::join('training_session_type', 'training_session_type.id', '=', 'training_session.training_session_type_id')
+                            ->select('training_session.*', 'training_session_type.name as type_name')
                             ->where(['workout_id' => $workout_id])
                             ->orderBy('day', 'asc')
                             ->get();
